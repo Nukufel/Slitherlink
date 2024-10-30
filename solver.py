fe116f5465f48e0f5576df0f18e476ee2476b85e
Niklas Vogel <niklas.vogel@bluewin.ch>
2024-10-30 20:10:54 +0100

    new solving with on line at a time

--- solver.py
+++ solver.py
@@ -1,88 +1,71 @@
 import random
 
-from settings import DIRECTIONS
+from settings import DIRECTIONS, GRID_ROWS, GRID_COLS
 from util import is_next_cell_valid
-VALID_COMBINATIONS = {
-    0: [[]],
-    1: [["top"], ["bottom"], ["left"], ["right"]],
-    2: [["top", "bottom"], ["left", "right"], ["top", "right"], ["top", "left"], ["bottom", "left"], ["bottom", "right"]],
-    3: [["top", "bottom", "left"], ["top", "bottom", "right"], ["top", "left", "right"], ["bottom", "left", "right"]],
-}
+
+MAX_CELLS = GRID_ROWS * GRID_COLS
 
 
 class Solver:
     def __init__(self, grid):
         self.grid = grid
+        self.stack = []
 
-    def solve(self, cells):
-        for cell in cells:
-            cell_number = cell.number
-            combinations = VALID_COMBINATIONS[cell_number]
-            self.set_boarders_and_crosses(combinations[0], cell)
-            if self.is_valid_combination(cell):
-                print(cell.row, cell.col, "is not valid")
+    def solve(self):
+        self.set_crosses_for_0()
+        for row in self.grid.cells:
+            for cell in row:
+                for pos in cell.get_empty_boarders():
+                    if self.is_valid_border(cell, pos):
+                        self.grid.set_boarder(cell, pos, True)
 
+    def set_crosses_for_0(self):
+        for row in self.grid.cells:
+            for cell in row:
+                if cell.number == 0:
+                    for direction in DIRECTIONS.keys():
+                        self.grid.set_boarder(cell, direction, False)
 
-    def is_valid_combination(self, cell):
-        for key, value in cell.borders.items():
-            if value:
-                r = cell.row
-                c = cell.col
-                positions = None
-                match key:
-                    case "top":
-                        positions = {
-                                (r-1, c): ("left", "right"),
-                                (r, c-1): ("top", "right"),
-                                (r, c+1): ("top", "left"),
-                                (r, c): ("right", "left")
-                            }
-                        pass
-                    case "left":
-                        positions = {
-                            (r-1, c): ("left", "bottom"),
-                            (r, c-1): ("top", "bottom"),
-                            (r+1, c): ("top", "left"),
-                            (r, c): ("top", "bottom")
-                        }
+    def is_valid_border(self, cell, pos):
+            r = cell.row
+            c = cell.col
+            positions = None
+            match pos:
+                case "top":
+                    positions = {
+                        (r - 1, c): ("left", "right"),
+                        (r, c - 1): ("top", "right"),
+                        (r, c + 1): ("top", "left"),
+                        (r, c): ("right", "left")
+                    }
+                    pass
+                case "left":
+                    positions = {
+                        (r - 1, c): ("left", "bottom"),
+                        (r, c - 1): ("top", "bottom"),
+                        (r + 1, c): ("top", "left"),
+                        (r, c): ("top", "bottom")
+                    }
 
-                    case "bottom":
-                        positions = {
-                            (r, c): ("left", "right"),
-                            (r, c-1): ("right", "bottom"),
-                            (r, c+1): ("bottom", "left"),
-                            (r-1, c): ("left", "right")
-                        }
-                    case "right":
-                        positions = {
-                            (r-1, c): ("right", "bottom"),
-                            (r, c+1): ("top", "bottom"),
-                            (r+1, c): ("top", "right"),
-                            (r, c): ("top", "bottom")
-                        }
+                case "bottom":
+                    positions = {
+                        (r, c): ("left", "right"),
+                        (r, c - 1): ("right", "bottom"),
+                        (r, c + 1): ("bottom", "left"),
+                        (r - 1, c): ("left", "right")
+                    }
+                case "right":
+                    positions = {
+                        (r - 1, c): ("right", "bottom"),
+                        (r, c + 1): ("top", "bottom"),
+                        (r + 1, c): ("top", "right"),
+                        (r, c): ("top", "bottom")
+                    }
 
-                return self.has_possible_boarder(cell, positions)
+            return self.has_possible_border(cell, positions)
 
-    def has_possible_boarder(self, cell, positions):
-        for key, value in positions.items():
-            if is_next_cell_valid(cell, key):
-                print("next cell is valid")
-                next_cell = self.grid.cells[key[0]][key[1]]
-                print(next_cell.row, next_cell.col)
-                for pos in value:
-                    print("pos")
-                    if next_cell.borders[pos] is True or next_cell.borders[pos] or None:
-                        print("ture")
-                        return True
-        return False
-
-    def set_boarders_and_crosses(self, combination, cell):
-        for pos in combination:
-            self.grid.set_boarder(cell, pos, True)
-            for boarder in cell.borders:
-                if not cell.borders[boarder]:
-                    self.grid.set_boarder(cell, pos, False)
-
+    def has_possible_border(self, cell, positions):
+        pass
 
 
 
