d75f6b82bb29e03b75c82e7cb1c8d5b6c8598f96
Niki <vogeln@bzz.ch>
2024-10-30 23:28:00 +0100

    has_possible_border but absolute shit

--- solver.py
+++ solver.py
@@ -16,6 +16,7 @@ class Solver:
         for row in self.grid.cells:
             for cell in row:
                 for pos in cell.get_empty_boarders():
+                    print(cell.row, cell.col)
                     if self.is_valid_border(cell, pos):
                         self.grid.set_boarder(cell, pos, True)
 
@@ -27,45 +28,53 @@ class Solver:
                         self.grid.set_boarder(cell, direction, False)
 
     def is_valid_border(self, cell, pos):
-            r = cell.row
-            c = cell.col
             positions = None
             match pos:
                 case "top":
                     positions = {
-                        (r - 1, c): ("left", "right"),
-                        (r, c - 1): ("top", "right"),
-                        (r, c + 1): ("top", "left"),
-                        (r, c): ("right", "left")
+                        (- 1, 0): ("left", "right"),
+                        (0, - 1): ("top", "right"),
+                        (0, 1): ("top", "left"),
+                        (0, 0): ("right", "left")
                     }
-                    pass
                 case "left":
                     positions = {
-                        (r - 1, c): ("left", "bottom"),
-                        (r, c - 1): ("top", "bottom"),
-                        (r + 1, c): ("top", "left"),
-                        (r, c): ("top", "bottom")
+                        (- 1, 0): ("left", "bottom"),
+                        (0,- 1): ("top", "bottom"),
+                        (1, 0): ("top", "left"),
+                        (0, 0): ("top", "bottom")
                     }
 
                 case "bottom":
                     positions = {
-                        (r, c): ("left", "right"),
-                        (r, c - 1): ("right", "bottom"),
-                        (r, c + 1): ("bottom", "left"),
-                        (r - 1, c): ("left", "right")
+                        (0, 0): ("left", "right"),
+                        (0, - 1): ("right", "bottom"),
+                        (0, 1): ("bottom", "left"),
+                        (- 1, 0): ("left", "right")
                     }
                 case "right":
                     positions = {
-                        (r - 1, c): ("right", "bottom"),
-                        (r, c + 1): ("top", "bottom"),
-                        (r + 1, c): ("top", "right"),
-                        (r, c): ("top", "bottom")
+                        (- 1, 0): ("right", "bottom"),
+                        (0, 1): ("top", "bottom"),
+                        (1, 0): ("top", "right"),
+                        (0, 1): ("top", "bottom")
                     }
 
             return self.has_possible_border(cell, positions)
 
     def has_possible_border(self, cell, positions):
-        pass
+        boarder_cntr = 0
+        for pos, borders in positions.items():
+            if is_next_cell_valid(cell, pos):
+                next_cell = self.grid.cells[cell.row + pos[0]][cell.col + pos[1]]
+                for border in borders:
+                    if next_cell.borders[border] is True or next_cell.borders[border] is None:
+                        boarder_cntr += 1
+        if boarder_cntr >= 4:
+            return True
+        return False
+
+
 
 
 
